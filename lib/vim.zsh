455f3277e1b864ff944bd6d42a7e8d8e6505f64c
Bradley Tse <bradleytse@gmail.com>
2013-12-25 01:25:48 -0500

    Changed the vim insert indicator and also its position on the prompt.

--- lib/vim.zsh
+++ lib/vim.zsh
@@ -1,4 +1,6 @@
 # key-bindings and settings associated with zsh vi mode
+#
+SEGMENT_SEPARATOR='⮀'
 
 # enable vi mode
 bindkey -v
@@ -11,7 +13,8 @@ function zle-keymap-select zle-line-init {
     #RPS1="${${KEYMAP/vicmd/-- NORMAL --}/(main|viins)/-- INSERT --}"
 
     # Adds a visual indicator to PROMPT
-    PROMPT="${${KEYMAP/vicmd/${ORIG_PROMPT}}/(main|viins)/%K{blue%F{red${VIM_INSERT}${ORIG_PROMPT}}"
+    PROMPT="${${KEYMAP/vicmd/${ORIG_PROMPT}${VIM_INSERT_MODE}}/(main|viins)/${ORIG_PROMPT}%F{red${VIM_INSERT_MODE}%f}"
+
 
     # Resets the prompt to show any changes to it
     zle reset-prompt
